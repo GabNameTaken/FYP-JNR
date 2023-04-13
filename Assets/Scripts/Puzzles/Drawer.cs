8be1fd9262262ba13a59685a7d75b5a5e8a16c74
InfynityRaz <203202U@mymail.nyp.edu.sg>
2023-04-13 10:36:43 +0800

    custom drawer button

--- Assets/Scripts/Puzzles/Drawer.cs
+++ Assets/Scripts/Puzzles/Drawer.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class Drawer : MonoBehaviour
 {
-
     [SerializeField] private Sprite[] img;
     [SerializeField] private GameObject image, screwdriver, note;
     private GameObject player;
--- Assets/Scripts/Puzzles/ReceptionBookcase/Bookcase.cs
+++ Assets/Scripts/Puzzles/ReceptionBookcase/Bookcase.cs
@@ -5,9 +5,9 @@ using UnityEngine.UI;
 
 public class Bookcase : InteractableObject
 {
-    [SerializeField] private GameObject popupBG, popupItems, roomItems, image, drawerItems, morsebook;
+    [SerializeField] GameObject popupBG, roomItems, image, drawerItems, morsebook;
     [SerializeField] GameObject[] otherGOs;
-    [SerializeField] private Sprite[] img;
+    [SerializeField] Sprite[] img;
     //private GameObject player;
 
     // Start is called before the first frame update
@@ -32,7 +32,6 @@ public class Bookcase : InteractableObject
     public void ClosePopup()
     {
         popupBG.SetActive(false);
-        popupItems.SetActive(false);
         roomItems.SetActive(true);
         for (int i = 0; i < otherGOs.Length; i++)
         {
@@ -40,10 +39,10 @@ public class Bookcase : InteractableObject
         }
         drawerItems.SetActive(false);
     }
+
     public override void OnClick()
     {
         popupBG.SetActive(true);
-        popupItems.SetActive(true);
         roomItems.SetActive(false);
         for (int i = 0; i < otherGOs.Length; i++)
         {
