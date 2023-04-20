66d8cb06046ea770719c9bc6a44d89081564c7f8
InfynityRaz <203202U@mymail.nyp.edu.sg>
2023-04-21 03:26:46 +0800

    mastermind base class
    in progress

---
+++ Assets/Scripts/Puzzles/Mastermind/Mastermind.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mastermind : MonoBehaviour
+{
+    [SerializeField] int attempts;
+    [SerializeField] int slots;
+
+    int[] slotNumbers;
+
+    int[] guesses;
+
+    
+}
