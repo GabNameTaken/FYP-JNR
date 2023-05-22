f3cdce2cc0e1aaaa13f7a9ceb7c7db6ffceee1c5
InfynityRaz <203202U@mymail.nyp.edu.sg>
2023-05-22 23:42:54 +0800

    quiz 1 done
    left asset textbox scaling

--- Assets/Scripts/Puzzles/Quiz/Quiz.cs
+++ Assets/Scripts/Puzzles/Quiz/Quiz.cs
@@ -1,9 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Quiz : MonoBehaviour
 {
-    public enum QuizOptions { A, B, C, D };
-    [SerializeField] QuizOptions answer;
+    [SerializeField] int answer;
+    [SerializeField] string[] optionText;
+
+    [SerializeField] TextMeshProUGUI correctText;
+    [SerializeField] GameObject correctPanel;
+    [SerializeField] TextMeshProUGUI incorrectText;
+    [SerializeField] GameObject incorrectPanel;
+
+    //To set navigation arrow active
+    [SerializeField] LockedDoor lockDoorRef;
+
+    public void PickOption(int option)
+    {
+        if (option == answer)
+        {
+            correctText.text = optionText[option];
+            correctPanel.SetActive(true);
+        }
+        else
+        {
+            incorrectText.text = optionText[option];
+            incorrectPanel.SetActive(true);
+        }
+    }
+
+    public void CloseCorrectPopup()
+    {
+        lockDoorRef.UnlockDoor();
+    }
+
+    public void CloseIncorrectPopup()
+    {
+        incorrectPanel.SetActive(false);
+    }
 }
