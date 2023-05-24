8a2af8978e3aafca59e2914e9a1dcfef540036ff
GabNameTaken <88995312+GabNameTaken@users.noreply.github.com>
2023-05-25 05:00:26 +0800

    final puzzle completed

--- Assets/Scripts/Puzzles/Quiz/InputFieldQuiz.cs
+++ Assets/Scripts/Puzzles/Quiz/InputFieldQuiz.cs
@@ -2,20 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
+using Photon.Pun;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
 
 public class InputFieldQuiz : MonoBehaviour
 {
     [SerializeField] TMP_InputField inputField;
     [SerializeField] string password;
+    [SerializeField] Image verificationImage;
+    [SerializeField] Sprite invalidVerificationImage;
+    [SerializeField] Sprite validVerificationImage;
     public void CheckInput()
     {
         if (inputField.text.ToUpper() == password.ToUpper())
         {
-            EndGame();
+            verificationImage.sprite = validVerificationImage;
+            GameSoundManager.instance.PlaySound("PCLogin");
+            StartCoroutine(EndGame());
+        }
+        else
+        {
+            verificationImage.sprite = invalidVerificationImage;
         }
     }
-    private void EndGame()
+    private IEnumerator EndGame()
     {
+        yield return new WaitForSeconds(3f);
 
+        object[] content = new object[] { true };
+        PhotonNetwork.RaiseEvent(RaiseEventManager.endGame, content, new RaiseEventOptions { Receivers = ReceiverGroup.All }, SendOptions.SendReliable);
     }
 }
